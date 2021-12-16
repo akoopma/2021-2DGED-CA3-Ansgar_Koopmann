32c7f50ae6f897aa8ec6fa511a80e79f086c3d9f
Ansgar Koopmann <koopmann@stud.hs-heilbronn.de>
2021-12-16 21:10:30 +0000

    Changed acceleration key to W

--- Games/LunarLander/js/Constants/Constants.js
+++ Games/LunarLander/js/Constants/Constants.js
@@ -3,7 +3,7 @@ class GameData {
     static SHIP_START_POSITION = new Vector2(20,20);
     static SHIP_START_VELOCITY = new Vector2(0,0);
     static SHIP_START_FACING = new Vector2(1, 0);
-    static SHIP_MOVE_KEYS = [Keys.A, Keys.D, Keys.Space];
+    static SHIP_MOVE_KEYS = [Keys.A, Keys.D, Keys.W];
     static SHIP_TURN_RATE = Math.PI;
     static SHIP_THRUST_ACCELERATION = 0.5;
     static SHIP_SPRITE_ROTATION_OFFSET = Math.PI/2;
